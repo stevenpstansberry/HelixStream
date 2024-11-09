6ac43733ebdeef81662da09f976426b4a20bc387
Steven <stevenpstansberry@gmail.com>
2024-11-08 23:35:28 -0800

    init springboot

---
+++ datastrand-analyzer/src/main/java/com/DataStrand/datastrand_analyzer/DatastrandAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.DataStrand.datastrand_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatastrandAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatastrandAnalyzerApplication.class, args);
+	}
+
+}
---
+++ datastrand-analyzer/src/test/java/com/DataStrand/datastrand_analyzer/DatastrandAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DataStrand.datastrand_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatastrandAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
