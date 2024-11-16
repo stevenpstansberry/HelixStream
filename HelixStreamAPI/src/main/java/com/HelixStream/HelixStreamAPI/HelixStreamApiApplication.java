8f8d691ba0c56a2833fa27e53b1f121aac138c4a
Steven <stevenpstansberry@gmail.com>
2024-11-16 14:02:22 -0800

    Added more detailed start up

--- HelixStreamAPI/src/main/java/com/HelixStream/HelixStreamAPI/HelixStreamApiApplication.java
+++ HelixStreamAPI/src/main/java/com/HelixStream/HelixStreamAPI/HelixStreamApiApplication.java
@@ -3,14 +3,25 @@ package com.HelixStream.HelixStreamAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class HelixStreamApiApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(HelixStreamApiApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(HelixStreamApiApplication.class, args);
-		System.out.println("hello");
-	}
 
+		logger.info("=======================================================");
+		logger.info("     HelixStream API Server Started Successfully!      ");
+		logger.info("=======================================================");
+		logger.info("Application Name: HelixStream API");
+		logger.info("Version: 1.0.0");
+		logger.info("Environment: {}", System.getProperty("spring.profiles.active", "default"));
+		logger.info("Listening on port: {}", System.getProperty("server.port", "8080"));
+		logger.info("Start Time: {}", java.time.LocalDateTime.now());
+		logger.info("=======================================================");
+	}
 }
